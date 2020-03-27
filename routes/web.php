92c5589c250b16575653041c692154249d01dc81
Sergey Strelchenko <serdjiostrel@gmail.com>
2020-03-28 01:00:38 +0200

    Update Link Statistic

--- routes/web.php
+++ routes/web.php
@@ -12,19 +12,40 @@
 */
 
 Route::get('/', function () {
-
-    $short_codes = \App\Link::inRandomOrder()->limit(5)->pluck('short_code');//value('short_code');//->get();//;
-
-//    dd($short_codes);
-    return view('welcome', ['short_codes' => $short_codes]);
+    $short_codes = \App\Link::inRandomOrder()->limit(5)->pluck('short_code');
+    return view('welcome', ['title' => 'Random Links', 'short_codes' => $short_codes]);
 });
 
 
 Route::get('/r/{code}', function ($code) {
 
-    $link = \App\Link::where('short_code', $code)->value('source_link');
-//    dd($link);
+    $link = \App\Link::where('short_code', $code)->get()->first();
 
-    return redirect($link);
-//    return view('welcome');
+    if($link === null)
+    {
+        return redirect('/');
+    }
+
+    $reader = new \GeoIp2\Database\Reader(resource_path() . '/GeoLite2/GeoLite2-City.mmdb');
+
+    try {
+        $record = $reader->city(request()->ip());
+    }
+    catch (\GeoIp2\Exception\AddressNotFoundException $exception) {
+        $record = $reader->city('88.214.10.164');
+    }
+
+    $statistic = new \App\Statistic();
+    $statistic->id = \Ramsey\Uuid\Uuid::uuid4()->toString();
+    $statistic->link_id = $link->id;
+    $statistic->ip = request()->ip();
+    $statistic->user_agent = request()->userAgent();
+    $statistic->country_name = $record->country->name;
+    $statistic->country_code = $record->country->isoCode;
+    $statistic->city_name = $record->city->name;
+    $statistic->save();
+
+//    dd($statistic);
+    return redirect($link->source_link);
+
 });
