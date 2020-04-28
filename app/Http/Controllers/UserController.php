d0d2785b54d49224a3090f596e66ae1db126e9f1
Sergey Strelchenko <serdjiostrel@gmail.com>
2020-04-28 23:01:45 +0300

    CRUD User Create

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,16 @@
 namespace App\Http\Controllers;
 
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class UserController
 {
     public function index()
     {
-        return view('admin.users.index', ['title' => 'Users', 'users' => \App\User::paginate(5)]);
+        return view('admin.users.index', ['title' => 'Users', 'users' => User::paginate(5)]);
     }
 
     public function create()
@@ -23,35 +26,38 @@ class UserController
         $request->validate([
             'name' => 'required',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|min:8',
-            'password_confirmation' => 'required|min:8|confirmed',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8',
         ]);
 
         $user = new User;
         $user->name = $request->get('name');
         $user->email = $request->get('email');
-        $user->email_verified_at = new \DateTime()-format('Y-m-d H:i:s');
+        $user->email_verified_at = (new \DateTime)->format('Y-m-d H:i:s');
         $user->password = Hash::make($request->get('password'));
+        $user->remember_token = Str::random(10);
 
         $user->save();
+
+        return redirect()->route("users.index");
     }
 
-    public function show(\App\User $user)
+    public function show(User $user)
     {
 
     }
 
-    public function edit(\App\User $user)
+    public function edit(User $user)
     {
 
     }
 
-    public function update(\App\User $user)
+    public function update(User $user)
     {
 
     }
 
-    public function destroy(\App\User $user)
+    public function destroy(User $user)
     {
 
     }
